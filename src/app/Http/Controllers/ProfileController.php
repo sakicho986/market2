a066830677b0e55d597dc6a18468950f6697e89b
saki cho <saki.cho.uriryu@gmail.com>
2024-09-18 13:14:12 +0900

    0918

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -23,7 +23,19 @@ class ProfileController extends Controller
 
     public function edit(Request $request)
     {
+        $request->validate([
+            'file' => 'required|file|mimes:jpg,png,pdf|max:2048',
+        ]);
+
+        // ファイルの保存
+        if ($request->file('avatar_file_name')) {
+            $file = $request->file('avatar_file_name');
+            $originalName = $file->getClientOriginalName();
+            $file->store('uploads', 'public'); // 'public' ストレージに保存
+        }
+
         $users = User::find($request->id);
+        $users->avatar_file_name = $originalName ?? null;
         $users->name = $request->name;
         $users->post = $request->post;
         $users->address = $request->address;
@@ -31,7 +43,5 @@ class ProfileController extends Controller
         $users->save();
 
         return view('profile', ['user' => $users]);
-
-        //        return redirect('/mypage');
     }
 }
